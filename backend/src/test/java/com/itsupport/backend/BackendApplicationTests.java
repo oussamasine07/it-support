2c5b310b81fbd6a29c4b0a3f2cd7e6ad6627ba24
oussamasine07 <sineoussama@gmail.com>
2025-06-10 15:12:00 +0100

    first commit and initialize backend

---
+++ backend/src/test/java/com/itsupport/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsupport.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
