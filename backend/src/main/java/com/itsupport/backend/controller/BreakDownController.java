759f6403d0f018b425eaee16e249d5c0584295c0
saidamst2003 <saida2003mst@gmail.com>
2025-06-11 12:58:45 +0100

    j'ai fait controller pour lister et afficher tous les panne

--- backend/src/main/java/com/itsupport/backend/controller/BreakDownController.java
+++ backend/src/main/java/com/itsupport/backend/controller/BreakDownController.java
@@ -1,13 +1,37 @@
 package com.itsupport.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.itsupport.backend.dto.BreakdownDTO;
+import com.itsupport.backend.model.BreakDown;
+import com.itsupport.backend.security.ServiceBreakDown;
+import com.itsupport.backend.service.BreakDownService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/BreakDown")
 @CrossOrigin
 public class BreakDownController {
 
+    private final BreakDownService breakDownService;
+    private final ServiceBreakDown serviceBreakDown;
+
+    public BreakDownController(BreakDownService breakDownService, ServiceBreakDown serviceBreakDown) {
+        this.breakDownService = breakDownService;
+        this.serviceBreakDown = serviceBreakDown;
+    }
+
+    //ajouter une panne
+    @PostMapping
+    public ResponseEntity<BreakDown> addBreakDown(@RequestBody BreakdownDTO breakdownDTO) {
+        BreakDown created = serviceBreakDown.addBreakDown(breakdownDTO);
+        return ResponseEntity.ok(created);
+    }
+    //Lister touter touter les pannes
+    @GetMapping("/{id}")
+    public ResponseEntity<List<BreakDown>> getBreakDownById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(serviceBreakDown.ListyAllBreakDown());
+    }
 
 }
