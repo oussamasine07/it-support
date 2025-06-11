356af3f5ab63e2cd5419dff2e263cb4fff9e3caa
saidamst2003 <saida2003mst@gmail.com>
2025-06-11 10:51:53 +0100

    j'ai creer repo BreakDown fonction pour listee Break Down

--- backend/src/main/java/com/itsupport/backend/repository/BreakDownRepo.java
+++ backend/src/main/java/com/itsupport/backend/repository/BreakDownRepo.java
@@ -4,4 +4,5 @@ import com.itsupport.backend.model.BreakDown;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BreakDownRepo extends JpaRepository<BreakDown, Integer> {
+
 }
--- backend/src/main/java/com/itsupport/backend/security/ServiceBreakDown.java
+++ backend/src/main/java/com/itsupport/backend/security/ServiceBreakDown.java
@@ -1,9 +1,24 @@
 package com.itsupport.backend.security;
 
+import com.itsupport.backend.model.BreakDown;
+import com.itsupport.backend.repository.BreakDownRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ServiceBreakDown {
 
+    private final BreakDownRepo breakDownRepo;
 
+
+    public ServiceBreakDown(
+        final BreakDownRepo breakDownRepo
+    ){
+      this.breakDownRepo = breakDownRepo;
+    }
+
+    public List<BreakDown> ListyAllBreakDown(){
+        return breakDownRepo.findAll();
+    }
 }
