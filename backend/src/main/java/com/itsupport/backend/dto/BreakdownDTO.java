17b37099b30710f64b58ba6f23c4eeba48bd54e7
saidamst2003 <saida2003mst@gmail.com>
2025-06-11 10:17:44 +0100

    j'ai creer DTO BreakDown

--- backend/src/main/java/com/itsupport/backend/dto/BreakdownDTO.java
+++ backend/src/main/java/com/itsupport/backend/dto/BreakdownDTO.java
@@ -1,6 +1,12 @@
 package com.itsupport.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record BreakdownDTO(
+        int id,
+
+        @NotBlank(message = "name is required")
+        String name
 
 ) {
 }
