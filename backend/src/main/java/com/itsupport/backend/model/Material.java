2314b81603ac0136126c73adf4f6f92a626de058
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-06-11 14:42:50 +0200

    create material model class

--- backend/src/main/java/com/itsupport/backend/model/Material.java
+++ backend/src/main/java/com/itsupport/backend/model/Material.java
@@ -1,4 +1,64 @@
 package com.itsupport.backend.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "materials")
+
 public class Material {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name" , nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "image", nullable = true)
+    private String image;
+
+    public Material() {}
+
+
+    //relations
+
+//    @ManyToOne
+//    @JoinTable(name = "ticket")
+//    private Ticket tickets;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
