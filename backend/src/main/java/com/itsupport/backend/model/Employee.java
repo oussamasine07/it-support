4414fb5a9f1c7b39fea323fb5aa657b088cdbe38
oussamasine07 <sineoussama@gmail.com>
2025-06-11 10:46:42 +0100

    create employee class

--- backend/src/main/java/com/itsupport/backend/model/Employee.java
+++ backend/src/main/java/com/itsupport/backend/model/Employee.java
@@ -1,4 +1,34 @@
 package com.itsupport.backend.model;
 
-public class Employee {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee extends User {
+
+    @Column(name = "post", nullable = false)
+    private String post;
+
+    @Column(name = "department", nullable = false)
+    private String deparment;
+
+    public Employee () {}
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public String getDeparment() {
+        return deparment;
+    }
+
+    public void setDeparment(String deparment) {
+        this.deparment = deparment;
+    }
 }
--- backend/src/main/java/com/itsupport/backend/model/User.java
+++ backend/src/main/java/com/itsupport/backend/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User {
 
     @Id
@@ -20,6 +21,37 @@ public abstract class User {
     @Column(name = "role", nullable = false)
     private Role role;
 
+    public User () {}
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
