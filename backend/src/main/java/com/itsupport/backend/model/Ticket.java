2c443d69bd4b98d426ed015fb00d8abde7502820
lahcen404 <lahcen.maskour2003@gmail.com>
2025-06-11 11:19:24 +0100

    Create Entity Ticket

--- backend/src/main/java/com/itsupport/backend/model/Ticket.java
+++ backend/src/main/java/com/itsupport/backend/model/Ticket.java
@@ -1,4 +1,71 @@
 package com.itsupport.backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Ticket {
+
+    @Id
+    private Long id;
+    private String description ;
+    private String image;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "employer_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "technician_id")
+    private Technician technician;
+
+    @ManyToOne
+    @JoinColumn(name = "breakdown_id")
+    private BreakDown breakDown;
+
+    @ManyToMany
+    @JoinTable(
+            name = "ticket_material",
+            joinColumns = @JoinColumn(name = "ticket_id"),
+            inverseJoinColumns = @JoinColumn(name = "material_id")
+    )
+    private Set<Material> materials = new HashSet<>();
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
