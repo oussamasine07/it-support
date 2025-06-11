10770eddeb29fd87e006a239a80bc91b75c22ffd
saidamst2003 <saida2003mst@gmail.com>
2025-06-11 12:15:25 +0100

    j'ai fait service de add breakdown

--- backend/src/main/java/com/itsupport/backend/model/BreakDown.java
+++ backend/src/main/java/com/itsupport/backend/model/BreakDown.java
@@ -14,4 +14,20 @@ public class BreakDown {
     private int id;
     @Column(name = "nom", nullable = false)
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- backend/src/main/java/com/itsupport/backend/security/ServiceBreakDown.java
+++ backend/src/main/java/com/itsupport/backend/security/ServiceBreakDown.java
@@ -1,7 +1,9 @@
 package com.itsupport.backend.security;
 
+import com.itsupport.backend.dto.BreakdownDTO;
 import com.itsupport.backend.model.BreakDown;
 import com.itsupport.backend.repository.BreakDownRepo;
+import org.hibernate.dialect.SpannerDialect;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -20,11 +22,29 @@ public class ServiceBreakDown {
       this.breakDownRepo = breakDownRepo;
     }
 
+         //lister toutes les pannes
+
     public List<BreakDown> ListyAllBreakDown(){
         return breakDownRepo.findAll();
     }
+
+        //aficher une SpannerDialect par ID
+
     public Optional<BreakDown> showBreakDown(int id){
         return breakDownRepo.findById(id);
     }
+
+       //to do :add token
+       // ajouter une panne
+
+    public BreakDown addBreakDown(BreakdownDTO breakdownDTO){
+        BreakDown breakDown = new BreakDown();
+        breakDown.setName(breakdownDTO.name());
+        return breakDownRepo.save(breakDown);
+
+        //Mettre a jour une panne
+    }
+
+
 
 }
