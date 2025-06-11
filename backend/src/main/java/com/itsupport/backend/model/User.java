59d930d237c807a0abf2c444c242a46d1f9ebfa6
oussamasine07 <sineoussama@gmail.com>
2025-06-11 10:22:27 +0100

    create user model

--- backend/src/main/java/com/itsupport/backend/model/User.java
+++ backend/src/main/java/com/itsupport/backend/model/User.java
@@ -1,4 +1,25 @@
 package com.itsupport.backend.model;
 
-public class User {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+
+
 }
