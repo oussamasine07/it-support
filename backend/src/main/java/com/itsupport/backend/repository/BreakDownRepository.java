9b57bda8bd15319f7755d1c1535a070d03937522
saidamst2003 <saida2003mst@gmail.com>
2025-06-12 12:06:10 +0100

    j'ai  creer une fonction repo de findbyusername

--- backend/src/main/java/com/itsupport/backend/repository/BreakDownRepository.java
+++ backend/src/main/java/com/itsupport/backend/repository/BreakDownRepository.java
@@ -5,7 +5,10 @@ import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BreakDownRepository extends JpaRepository<BreakDown, Integer> {
     List<BreakDown> findByUser(User user);
+    Optional<User> findByUsername(String username);
+
 }
