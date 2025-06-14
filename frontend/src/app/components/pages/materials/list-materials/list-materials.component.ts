eda4a00c5d1848a31e0c1531b3cba99a47d2f4f5
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-06-14 13:33:58 +0200

    add list material in front

---
+++ frontend/src/app/components/pages/materials/list-materials/list-materials.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-materials',
+  imports: [],
+  templateUrl: './list-materials.component.html',
+  styleUrl: './list-materials.component.css'
+})
+export class ListMaterialsComponent {
+
+}
