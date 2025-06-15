336dc446e6a1fd308f0cb82fbe60a33ef7272222
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-06-15 22:39:30 +0200

    terminer la partie affichage dynamique des materials

--- frontend/src/app/components/pages/materials/list-materials/list-materials.component.ts
+++ frontend/src/app/components/pages/materials/list-materials/list-materials.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MaterialService} from '../../../../services/material.service';
+import {Material} from '../../../../models/interfaces/material';
 
 @Component({
   selector: 'app-list-materials',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './list-materials.component.html',
   styleUrl: './list-materials.component.css'
 })
-export class ListMaterialsComponent {
+
+export class ListMaterialsComponent implements OnInit {
+  materials: Material[] = [];
+
+  constructor(private materialService: MaterialService) {}
+
+  ngOnInit(): void {
+    this.materialService.getMaterials().subscribe((data) => {
+      this.materials = data;
+    });
+  }
 
 }
